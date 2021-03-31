bccca375e2763534293a07865d0224b0b0ebb8e1
p0wa5 <jeffrey.freiwald98@gmail.com>
2021-03-31 14:16:37 +0200

    Add --all

---
+++ rpcontacts_project/rpcontacts/model.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-'
+#rpcontacts/model.py
+
+#This modules provides a model to manage the contacts table
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtSql import QSqlTableModel
+
+class ContactsModel:
+	def __init__(self):
+		self.model = self._createModel()
+
+	@staticmethod
+	def _createModel():
+		#create an set up the model
+		tableModel = QSqlTableModel()
+		tableModel.setTable("contacts")
+		tableModel.setEditStrategy(QSqlTableModel.OnFieldChange)
+		tableModel.select()
+		headers = ("ID", "Name", "JOB", "Email")
+		for columnIndex, header in enumerate(headers):
+			tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
+		return tableModel
+	
